9c1911f92da596b28b6c4f0bf71dea3ad3ac2a2d
pubudiniashani <pubudiniashani2000@gmail.com>
2024-10-09 08:54:45 +0530

    customer entity

--- src/main/java/lk/ijse/poswithspringbackend/entity/impl/CustomerEntity.java
+++ src/main/java/lk/ijse/poswithspringbackend/entity/impl/CustomerEntity.java
@@ -1,4 +1,22 @@
 package lk.ijse.poswithspringbackend.entity.impl;
 
-public class CustomerEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.poswithspringbackend.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "customer")
+public class CustomerEntity implements SuperEntity {
+    @Id
+    private String customerId;
+    private String customerName;
+    private String address;
+    private String contactNumber;
 }
