b43179e6daf1dbc40b2d75d894485e4d7128aca2
pubudiniashani <pubudiniashani2000@gmail.com>
2024-10-09 22:35:34 +0530

    regex add

--- src/main/java/lk/ijse/poswithspringbackend/util/RegexProcess.java
+++ src/main/java/lk/ijse/poswithspringbackend/util/RegexProcess.java
@@ -1,4 +1,12 @@
 package lk.ijse.poswithspringbackend.util;
 
+import java.util.regex.Pattern;
+
 public class RegexProcess {
+
+    public static boolean customerIdMatch(String customerId){
+        String regexCusId = "^C\\d{3,}$\n";
+        Pattern regexPattern = Pattern.compile(regexCusId);
+        return regexPattern.matcher(customerId).matches();
+    }
 }
